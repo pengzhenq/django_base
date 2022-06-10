026d3918339babd3c09d9be2738c73479d4b90a3
PZQ <2235902747@qq.com>
2022-06-10 15:32:48 +0800

    站点管理

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class BookInfo(models.Model):
     # 创建字段，字段类型...
     name = models.CharField(max_length=10)
-
+    def __str__(self):
+        return self.name
 # 准备人物列表信息的模型类
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=10)
